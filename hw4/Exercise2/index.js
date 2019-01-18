f8e271c51053651e600c1b288dfde6661b634bf1
Laith Mihyar <LaithMihyar@laiths-mbp.cs.mum.edu>
2019-01-18 14:59:59 -0600

    push hw4

--- hw4/Exercise2/index.js
+++ hw4/Exercise2/index.js
@@ -3,24 +3,24 @@ const { fork } = require('child_process')
 const url = require('url');
 const path = require('path')
 
-http.createServer().on('request', (req,res) => {
+
+http.createServer().on('request', (req, res) => {
     const filePath = url.parse(req.url, true).query.url;
 
-    var childProcess = fork(path.join(__dirname,'readFileChild.js'),[filePath],{silent:true, execArgv:['--inspect']});
-    childProcess.stdout.on('data', data => {
-        console.log(data)
-        res.write(data.toString());
+    var childProcess = fork(path.join(__dirname, 'readFileChild.js'));
+
+    childProcess.send(filePath)
+
+    childProcess.on('message', (message) => {
+        console.log(message)
+        if (message !== 'asaad') {
+            res.write(message);
+        } else {
+            console.log('been end')
+            res.end();
+        }
     });
 
-    childProcess.stderr.on('error', error => {
-        console.log('Error: '+error)
-        res.end(error.toString());
-    });
-
-    childProcess.on('exit', err => {
-        console.log(`Child exited with code ${err}`);
-        res.end();
-    });
 }).listen(3000);
 
 
--- hw4/Exercise2/readFileChild.js
+++ hw4/Exercise2/readFileChild.js
@@ -1,4 +1,22 @@
 const fs = require('fs')
+const path = require('path')
 
-const readStream = fs.createReadStream(process.argv[2]);
-readStream.pipe(process.stdout);
+
+var readfile = function(pathName){
+    const readStream = fs.createReadStream(pathName);
+    readStream.on("data", (data) => {
+        // console.log('chunck: '+data)
+        process.send(data.toString());
+    })
+    
+    readStream.on("end", (end) => {
+        // console.log('end')
+        process.send("asaad");
+    })
+}
+
+process.on('message', file =>{
+    var filename = path.join(__dirname, file);
+    readfile(filename);
+})
+
